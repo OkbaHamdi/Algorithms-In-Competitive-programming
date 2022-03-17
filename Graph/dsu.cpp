b2dd147e0a2ae280c4b0755459dbfe9c1bcaf9b5
Okba Hamdi <94719238+OkbaHamdi@users.noreply.github.com>
2022-03-17 10:09:36 +0100

    Create dsu.cpp

---
+++ Graph/dsu.cpp
@@ -0,0 +1,22 @@
+//class
+class dsu{
+     vl root, groupsize;
+
+    dsu(ll n)
+    {
+        groupsize.resize(n + 1, 1);
+        root.resize(n + 1);
+        for (ll i = 1; i < n; i++) root[i] = i;
+    }
+
+    ll find_root(ll u)  {return (root[u] == u) ? u : root[u] = find_root(root[u]);}
+    void join(ll u,ll v){
+        u = find_root(u);
+        v = find_root(v);
+        if (groupsize[u] < groupsize[v]) swap(u, v);
+        root[v] = u;
+        groupsize[u] += groupsize[v];
+    }
+
+    bool together(ll u, ll v){return find_root(u)==find_root(v)?true:false;}
+};
