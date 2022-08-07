14828c2b846ab5d62d85864110ead27b2848f8cf
Okba Hamdi <94719238+OkbaHamdi@users.noreply.github.com>
2022-08-07 09:54:15 +0100

    Update dsu.cpp

--- Graph/dsu.cpp
+++ Graph/dsu.cpp
@@ -1,59 +1,32 @@
-//implementaton with  struct
-//commun version
-typedef long long ll;
-struct dsu{
-     vector<ll> root, groupsize;
+vector<int> root, sz;
+struct dsu
+   {
 
-    dsu(ll n)
-    {
-        groupsize.resize(n + 1, 1);
-        root.resize(n + 1);
-        for (ll i = 1; i <=n; i++) root[i] = i;
-    }
+      dsu(int n)
+      {
+         sz.resize(n + 1, 1);
+         root.resize(n + 1);
+         for (int i = 1; i <= n; i++) root[i] = i;
+      }
 
-    ll find_root(ll u)  {return (root[u] == u) ? u : root[u] = find_root(root[u]);}
-    void join(ll u,ll v){
-        u = find_root(u);
-        v = find_root(v);
-        if(u==v)return;
-        if (groupsize[u] < groupsize[v]) swap(u, v);
-        root[v] = u;
-        groupsize[u] += groupsize[v];
-    }
+      int find(int u){
+         return (root[u] == u ? u : root[u] = find(root[u])) ; 
+      }
+         
+      bool same(int u, int v)
+      {
 
-    bool together(ll u, ll v){return find_root(u)==find_root(v)?true:false;}
-};
-// version to get min and max of the nodes easily
+         if (find(u) == find(v)) return true;
+         return false;
+      }
 
-typedef long long ll;
-struct dsu{
-     vl root;
-     vector<tuple<ll,ll,ll>>grp;
 
-    dsu(ll n)
-    {
-        grp.resize(n + 1);
-        root.resize(n + 1);
-
-        for (ll i = 1; i <= n; i++) {
-                root[i] = i;
-                get<0>(grp[i])=1;
-                get<1>(grp[i])=i;
-                get<2>(grp[i])=i;
-        }
-    }
-
-    ll find_root(ll u)  {return (root[u] == u) ? u : root[u] = find_root(root[u]);}
-    void join(ll u,ll v){
-        u = find_root(u);
-        v = find_root(v);
-        if(u==v)return;
-        if (get<0>(grp[u])< get<0>(grp[v])) swap(u, v);
-        root[v] = u;
-        get<0>(grp[u]) +=get<0> (grp[v]);
-        get<1>(grp[u])=min(get<1>(grp[u]),get<1>(grp[v]));
-        get<2>(grp[u])=max(get<2>(grp[u]),get<2>(grp[v]));
-    }
-
-    bool together(ll u, ll v){return find_root(u)==find_root(v)?true:false;}
-};
+      void join(int u, int v){
+         u = find(u);
+         v = find(v);
+         if (sz[u] < sz[v]) swap(u, v);
+         root[v] = u;
+         sz[u] += sz[v];
+      }
+ };
+// create an instance : dsu ds(n) 
