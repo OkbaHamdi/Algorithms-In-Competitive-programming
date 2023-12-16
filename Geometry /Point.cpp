8c2d9ec7261fe7f03d6f763023b60ab130a7c72e
Okba HAMDI <94719238+OkbaHamdi@users.noreply.github.com>
2023-12-16 11:45:47 +0100

    Update Point.cpp

--- Geometry /Point.cpp
+++ Geometry /Point.cpp
@@ -1,31 +1,31 @@
-struct point{
-    int x,y;
-    point(int kx=0,int ky=0){
-        x=kx;
-        y=ky;
+template<typename T>
+struct Point {
+    T x,y;
+    Point(T xx=0,T yy=0):x(xx),y(yy){}
+    //cin
+    friend istream& operator>>(istream& input, Point<T>& p) {
+        input>>p.x>>p.y;
+        return input;
     }
+    //cout
+    friend ostream& operator<<(ostream& output, const Point<T>& p) {
+        output<<"("<<p.x<<","<<p.y<<")";
+        return output;
+    }
+    Point<T> operator + (Point<T>p){return{x+p.x, y+p.y};}
+    Point<T> operator - (Point<T>p){return{x-p.x, y-p.y};}
+    Point<T> operator * (Point<T>p){return{x*p.x-y*p.y, x*p.y+y*p.x};}
+    Point<T> operator * (T p){return{x*p, y*p};}
+    Point<T> operator / (T p){return{x/p, y/p};}
+    bool operator == (const Point<T> &p)const{return x==p.x && y == p.y;}
+    bool operator != (const Point<T> &p)const{return !(*this == p);}
+    bool operator <  (const Point<T> &p)const{return x < p.x || (x == p.x && y < p.y);}
+    bool operator >  (const Point<T> &p)const{return x > p.x || (x == p.x && y > p.y);}
+    T norm(){return x*x+y*y;}
+    T norm(Point<T> p){return abs(p.x-x)*abs(p.x-x) + abs(p.y-y)*abs(p.y-y);}
+    T dot(Point<T> p){return x*p.x + y*p.y;}//a*cos(O)b
+    T cross(Point<T> p){return x*p.y-y*p.x;}
+    T orient(Point<T> b,Point<T> c){return (b-*this).cross(c-*this);}
+    ld dist(){return sqrt(norm());}
+    ld dist(Point<T> p){return sqrt(norm(p));}
 };
- bool colin(point a,point b,point c){
-        ld test=a.x*(b.y-c.y)+b.x*(c.y-a.y)+c.x*(a.y-b.y);
-        if(test==0)return 1;
-        return 0;
-}
-vector<point>f(point a,point b,vector<point>v){
-    vector<point>ans;
-    for(auto c:v){
-        if(!colin(a,b,c))ans.pb(c);
-    }
-    return ans;
-}
-// segments of n points
-bool burn(vector<point>v,int n){
-    if(v.size()<=2*n)return 1;
-    if(n==0)return 0;
-    bool ok=0;
-    for(int i=0;i<2;i++)for(int j=i+1;j<=i+2;j++){
-        auto x=f(v[i],v[j],v);
-        ok=burn(x,n-1);
-    }
-    return ok;
-
-}
