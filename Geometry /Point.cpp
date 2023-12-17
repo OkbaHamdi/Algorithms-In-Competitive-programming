80356a08377c9e699b992a64a8ec82b2d3d0f24b
Okba HAMDI <94719238+OkbaHamdi@users.noreply.github.com>
2023-12-17 18:44:39 +0100

    Update Point.cpp

--- Geometry /Point.cpp
+++ Geometry /Point.cpp
@@ -25,14 +25,22 @@ struct Point {
     T norm(Point<T> p){return abs(p.x-x)*abs(p.x-x) + abs(p.y-y)*abs(p.y-y);}
     T dot(Point<T> p){return x*p.x + y*p.y;}//a*cos(O)b
     T cross(Point<T> p){return x*p.y-y*p.x;}
-    T orient(Point<T> b,Point<T> c){return (b-*this).cross(c-*this);}
+    T orient(Point<T> b,Point<T> c){return (b-*this).cross(c-*this);}//cross 3 points
     ld dist(){return sqrt(norm());}
     ld dist(Point<T> p){return sqrt(norm(p));}
     // slope
     friend int direction(Point<T>a,Point<T>b,Point<T> c){
-        ld val=(b.y-a.y)*(c.x - b.x)-(b.x-a.x)*(c.y-b.y);
+        ld val=(b.y-a.y)*(c.x-b.x)-(b.x-a.x)*(c.y-b.y);
         if(val==0)return 0;//collinear
-        if(val<0)return -1;//Anti-clockwise direction c is left (or up)
+        if(val<0)return 2;//Anti-clockwise direction c is left (or up)
         return 1;// Clockwise direction c is right (or down)
+    }
+    // angle bAc
+    friend ld angle(Point<T>a,Point<T>b,Point<T>c) {
+        ld ux=b.x-a.x;
+        ld uy=b.y-a.y;
+        ld vx=c.x-a.x;
+        ld vy=c.y-a.y;
+        return acos((ux*vx+uy*vy)/sqrt((ux*ux+uy*uy)*(vx*vx+vy*vy)));
     }
 };
